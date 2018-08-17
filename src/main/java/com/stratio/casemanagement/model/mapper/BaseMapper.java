e4c0cba63e6486a2619bfaf292fbfd74faf33b79
Javier Sanz <jsanz@stratio.com>
2018-08-17 11:50:21 +0200

    Add raw attachments data for GET method

--- src/main/java/com/stratio/casemanagement/model/mapper/BaseMapper.java
+++ src/main/java/com/stratio/casemanagement/model/mapper/BaseMapper.java
@@ -2,9 +2,7 @@ package com.stratio.casemanagement.model.mapper;
 
 import java.time.LocalDateTime;
 
-public interface SimpleMapper<A, B> {
-    B mapAToB(A a);
-
+public interface BaseMapper {
     default String localDateTimeToString(LocalDateTime localDateTime) {
         return localDateTime == null ? null : localDateTime.toString();
     }
